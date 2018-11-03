3af69ba89ac8dc54df2a521becc2799459e8dc60
roman <roman@roman-ws.kabelnet.net>
2018-11-03 13:30:19 +0100

    allow all domains in cors

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     // should do a check here to match $_SERVER['HTTP_ORIGIN'] to a
     // whitelist of safe domains
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header("Access-Control-Allow-Origin: *");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
